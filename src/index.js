3c0fe041746797a6291e77b13c2b51c99966a386
perroentucasa <86454571+perroentucasa@users.noreply.github.com>
2021-08-30 13:19:38 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -4,7 +4,26 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+  expr = expr.replace(/ /g, ''); 
+  if (expr.indexOf('/0') !== -1)
+  
+    throw new Error('Can not divide by zero.');
+
+  
+  let brackets = 0;
+  expr.split('').forEach((el) => {
+    el === '(' ? brackets++ : false;
+    el === ')' ? brackets-- : false;
+  });
+
+  if (brackets !== 0)
+  
+    throw new Error('Brackets are not paired');
+
+  
+  expr = new Function('return ' + expr);
+
+    return expr();
 }
 
 module.exports = {
